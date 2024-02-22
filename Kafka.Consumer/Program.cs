75531a2e6df54f695e08530439b31951362bacd9
mmostofakamal <mmostofa@volvocars.com>
2024-02-22 12:09:02 +0100

    Added kafka producer and consumer

---
+++ Kafka.Consumer/Program.cs
@@ -0,0 +1,57 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.Consumer
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var conf = new ConsumerConfig
+            {
+                BootstrapServers = "PLAINTEXT://localhost:29092",
+                SecurityProtocol = SecurityProtocol.Plaintext,
+                GroupId = "Demo_consumer_"+Guid.NewGuid(),
+                ClientId = "Demo_client",
+                AutoOffsetReset = AutoOffsetReset.Earliest,
+                EnableAutoCommit = false
+            };
+
+            using var c = new ConsumerBuilder<string, string>(conf).Build();
+            c.Subscribe("test.poc");
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cts.Cancel();
+            };
+            while (true)
+            {
+                var cr = c.Consume(cts.Token);
+                try
+                {
+                    try
+                    {
+                        Console.WriteLine($"Consumed message '{cr.Message.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        if (cr.Message.Value == "error")
+                        {
+                            throw new ArgumentException("Some intentional error");
+                        }
+
+                        c.Commit(cr);
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Console.WriteLine($"Error occurred: {e.Error.Reason}");
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error occurred: " + ex);
+                }
+
+            }
+        }
+    }
+}
---
+++ Kafka.Producer/Program.cs
@@ -0,0 +1,33 @@
+﻿using Confluent.Kafka;
+
+namespace Kafka.Producer
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "PLAINTEXT://localhost:29092",
+                SecurityProtocol = SecurityProtocol.Plaintext,
+            };
+
+            using var producer = new ProducerBuilder<string, string>(config).Build();
+            string topic = "test.poc";
+
+            while (true)
+            {
+                Console.Write("Enter your message: ");
+                var msg = Console.ReadLine();
+                var message = new Message<string, string>
+                {
+                    Value = msg,
+                    Key = msg
+                };
+
+                var result = await producer.ProduceAsync(topic, message);
+              Console.WriteLine(result.Status);
+            }
+        }
+    }
+}
